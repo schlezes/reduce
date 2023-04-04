adfe48b45684b2703e262cc613d7d86e43791d31
schlezes <schlezes@hotmail.com>
2023-04-04 14:12:58 -0700

    fix array value to array element

--- return-b.js
+++ return-b.js
@@ -1,12 +1,12 @@
 /*
   Take any array
 
-  we just want the final value of the array
+  we just want the final element of the array
 
     we do not care about slope
-    we do not care about any sub array
-    we do not care about any duplicate values
-    we do not care about the sum of the values of the array
+    we do not care about any sub elements
+    we do not care about any duplicate elements
+    we do not care about the sum of the elements of the array
 
   we can use reduce and just return the final b
 
@@ -19,9 +19,8 @@ const y = [4, 2, -1, 24, 10, 9]
 console.log(y.reduce((a, b) => {return b}, 0));
 
 /*
-  if we want the sum of values
+  if we want the sum of elements
   use: y.reduce((a,b) => (a + b), 0)
 */
-
 
 console.log(y.reduce((a,b) => (a + b), 0))
